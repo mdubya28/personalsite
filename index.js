2d4093516c8a1c3dba997d31c583e20af6a5ead8
mdubya28 <68699218+mdubya28@users.noreply.github.com>
2020-10-17 20:19:27 -0600

    update to xp circle click action

--- index.js
+++ index.js
@@ -1,14 +1,69 @@
+var display = false;
+
 $(".experience-circle").click(function(){
         var clicked = $(this).attr("id");
-        $(".expand-"+ clicked).slideToggle();
 
-        if (!$(".experience-circle").hasClass('experience-circle-clicked')) {
+        if ($(".expand-" + clicked).is(":visible")) {
+                $(".expand-" + clicked).slideUp();
+                $(this).removeClass("experience-circle-clicked");
+                display = false;
+        }
+
+        if (display === true){
+                $(".xp").slideUp();
+                $(".experience-circle").removeClass("experience-circle-clicked");
+                display = false;
+
+                displayXp(clicked);
                 $(this).addClass("experience-circle-clicked");
         }
-        else {
-                $(this).removeClass("experience-circle-clicked");
-        }      
-});
+        
+        
+        if (display === false) {
+                displayXp(clicked);
+                $(this).addClass("experience-circle-clicked");
+        }
+
+        
         
 
-  
+});
+
+function displayXp(name) {
+        $(".expand-"+ name).slideDown();
+        display = true;
+}
+
+
+//         if (display = true) {
+//                 $(".xp").slideUp
+//                 $(clicked).removeClass("experience-circle-clicked");
+//                 display = false;
+//         }
+//         if (display = false) {
+//                 displayXp();
+//                 $(this).addClass("experience-circle-clicked");
+//                 display = true;
+//         }
+// });
+
+
+
+
+        // if (!$(".experience-circle").hasClass('experience-circle-clicked')) {
+        //         $(this).addClass("experience-circle-clicked");
+        // }
+        // else {
+        //         $(this).removeClass("experience-circle-clicked");
+        // }      
+
+
+
+//         if ($(".expand-" + clicked).is(":visible")) {
+        //         $(".expand-" + clicked).slideUp();
+        //         $(this).removeClass("experience-circle-clicked");
+                // display = false;
+
+
+
+
